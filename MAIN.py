8e381daeb8f66c78f20d274029835dc0ce36660a
Johann Cervantes <BAAL3113@GMAIL.COM>
2025-08-28 22:06:16 -0600

    Create MAIN.py

---
+++ MAIN.py
@@ -0,0 +1,13 @@
+from FUNCION_FIBONACCI import fibonacci_hasta
+from COMANDOS_BASICOS_DE_PYTHON import saludar
+
+def main():
+    print("Bienvenido al programa de consola")
+    saludar("JOHANN")  # Si tienes la función saludar disponible
+    print("Secuencia de Fibonacci:")
+    fibonacci_hasta(10)
+    # print(frutas)
+    # print(persona)
+
+if __name__ == "__main__":
+    main()
