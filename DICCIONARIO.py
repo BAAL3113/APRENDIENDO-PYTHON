2e3587cb13a75112780a88ca8fbdbf92da5a37d3
Johann Cervantes <BAAL3113@GMAIL.COM>
2025-09-03 21:49:49 -0600

    Create DICCIONARIO.py

---
+++ DICCIONARIO.py
@@ -0,0 +1,7 @@
+personas = [
+    {"nombre": "Ana", "edad": 25, "sexo": "F", "gusto_de_musica": "Rock"},
+    {"nombre": "Luis", "edad": 30, "sexo": "M", "gusto_de_musica": "Jazz"},
+    {"nombre": "María", "edad": 22, "sexo": "F", "gusto_de_musica": "Pop"},
+    {"nombre": "Carlos", "edad": 28, "sexo": "M", "gusto_de_musica": "Reggaetón"},
+    {"nombre": "Sofía", "edad": 27, "sexo": "F", "gusto_de_musica": "Clásica"}
+]
