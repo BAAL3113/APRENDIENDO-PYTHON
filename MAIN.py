c2dcb43093b61e0f99721423f4ed84059bb059cb
Johann Cervantes <BAAL3113@GMAIL.COM>
2025-09-03 21:49:53 -0600

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,13 +1,16 @@
 from FUNCION_FIBONACCI import fibonacci_hasta
-from COMANDOS_BASICOS_DE_PYTHON import saludar
-
+from DICCIONARIO import personas
 def main():
     print("Bienvenido al programa de consola")
-    saludar("JOHANN")  # Si tienes la función saludar disponible
     print("Secuencia de Fibonacci:")
     fibonacci_hasta(10)
-    # print(frutas)
-    # print(persona)
-
+    print("\nLista de personas:")
+    for persona in personas:
+        if persona["gusto_de_musica"] == "Rock" or persona["gusto_de_musica"] == "Reggaetón":
+            print(f"Nombre: {persona['nombre']}, Edad: {persona['edad']}, Sexo: {persona['sexo']}, Gusto de música: {persona['gusto_de_musica']}")
+            if persona["gusto_de_musica"] == "Rock": 
+                print({persona["nombre"]}, "tiene buen gusto musical")
+            elif persona["gusto_de_musica"]== "Reggaetón":
+                print({persona["nombre"]}, "no sirves para nada")
 if __name__ == "__main__":
     main()
